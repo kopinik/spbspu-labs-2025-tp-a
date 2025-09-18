752b71bb0a2d971942688c65b249f338906e30bc
kopinik <bk65763980@mail.ru>
2025-09-18 17:24:58 +0300

    added Xref

--- sherkunov.daniil/F0/main.cpp
+++ sherkunov.daniil/F0/main.cpp
@@ -1,1 +1,13 @@
-int main() {}
+#include "Xref.h"
+
+int main(int argc, char** argv) {
+    sherkunov::CrossReferenceSystem system;
+
+    int result = sherkunov::processCommandLineArguments(argc, argv, system);
+    if (result != -1) {
+        return result;
+    }
+
+    sherkunov::runInteractiveMode(system);
+    return 0;
+}
