9707ddb526a4362f130a0b232e2f730eef10d395
kopinik <bk65763980@mail.ru>
2025-10-23 13:12:50 +0300

    Fixed tests

--- sherkunov.daniil/F0/Xref.cpp
+++ sherkunov.daniil/F0/Xref.cpp
@@ -1,9 +1,13 @@
 #include "Xref.h"
 
-namespace sherkunov {
+namespace sherkunov
+{
 
-bool CrossReferenceSystem::isValidName(const std::string& name) {
-  if (name.empty()) return false;
+bool CrossReferenceSystem::isValidName(const std::string &name)
+{
+  if (name.empty()) {
+    return false;
+  }
   for (char c : name) {
     if (!std::isalnum(c) && c != '_' && c != '-') {
       return false;
@@ -12,8 +16,11 @@ bool CrossReferenceSystem::isValidName(const std::string& name) {
   return true;
 }
 
-bool CrossReferenceSystem::isValidWord(const std::string& word) {
-  if (word.empty()) return false;
+bool CrossReferenceSystem::isValidWord(const std::string &word)
+{
+  if (word.empty()) {
+    return false;
+  }
   for (char c : word) {
     if (!std::isalpha(c)) {
       return false;
@@ -22,8 +29,9 @@ bool CrossReferenceSystem::isValidWord(const std::string& word) {
   return true;
 }
 
-void CrossReferenceSystem::buildReferences(const std::string& text_name, const std::string& content) {
-  TextData& data = texts[text_name];
+void CrossReferenceSystem::buildReferences(const std::string &text_name, const std::string &content)
+{
+  TextData &data = texts[text_name];
   data.content = content;
   data.references.clear();
 
@@ -33,7 +41,8 @@ void CrossReferenceSystem::buildReferences(const std::string& text_name, const s
 
   while (iss >> word) {
     word.erase(std::remove_if(word.begin(), word.end(),
-             [](char c) { return !std::isalpha(c); }), word.end());
+                              [](char c) { return !std::isalpha(c); }),
+               word.end());
 
     if (!word.empty()) {
       std::transform(word.begin(), word.end(), word.begin(), ::tolower);
@@ -43,7 +52,8 @@ void CrossReferenceSystem::buildReferences(const std::string& text_name, const s
   }
 }
 
-void CrossReferenceSystem::build(const std::string& text_name, const std::string& text_content) {
+void CrossReferenceSystem::build(const std::string &text_name, const std::string &text_content)
+{
   if (!isValidName(text_name)) {
     throw std::runtime_error("<INVALID NAME>");
   }
@@ -57,13 +67,14 @@ void CrossReferenceSystem::build(const std::string& text_name, const std::string
   buildReferences(text_name, text_content);
 }
 
-void CrossReferenceSystem::reconstruct(const std::string& text_name, const std::string& output_file) {
+void CrossReferenceSystem::reconstruct(const std::string &text_name, const std::string &output_file)
+{
   auto it = texts.find(text_name);
   if (it == texts.end()) {
     throw std::runtime_error("<NOT FOUND>");
   }
 
-  const TextData& data = it->second;
+  const TextData &data = it->second;
   std::istringstream iss(data.content);
   std::vector<std::string> words;
   std::string word;
@@ -72,7 +83,7 @@ void CrossReferenceSystem::reconstruct(const std::string& text_name, const std::
     words.push_back(word);
   }
 
-  for (const auto& ref : data.references) {
+  for (const auto &ref : data.references) {
     for (size_t pos : ref.second) {
       if (pos >= words.size()) {
         throw std::runtime_error("<CORRUPTED INDEX>");
@@ -95,11 +106,12 @@ void CrossReferenceSystem::reconstruct(const std::string& text_name, const std::
   }
 }
 
-void CrossReferenceSystem::concat(const std::string& new_name, const std::string& name1, const std::string& name2) {
+void CrossReferenceSystem::concat(const std::string &new_name, const std::string &name1, const std::string &name2)
+{
   if (!isValidName(new_name)) {
     throw std::runtime_error("<INVALID NAME>");
   }
-  if (texts.find(new_name) != texts.end()) {
+  if (texts.find(new_name) != std::end(texts)) {
     throw std::runtime_error("<ALREADY EXISTS>");
   }
 
@@ -116,7 +128,8 @@ void CrossReferenceSystem::concat(const std::string& new_name, const std::string
   buildReferences(new_name, combined_content);
 }
 
-std::vector<size_t> CrossReferenceSystem::search(const std::string& text_name, const std::string& word) {
+std::vector<size_t> CrossReferenceSystem::search(const std::string &text_name, const std::string &word)
+{
   auto it = texts.find(text_name);
   if (it == texts.end()) {
     throw std::runtime_error("<NOT FOUND>");
@@ -128,7 +141,7 @@ std::vector<size_t> CrossReferenceSystem::search(const std::string& text_name, c
   std::string search_word = word;
   std::transform(search_word.begin(), search_word.end(), search_word.begin(), ::tolower);
 
-  const auto& refs = it->second.references;
+  const auto &refs = it->second.references;
   auto ref_it = refs.find(search_word);
   if (ref_it != refs.end()) {
     return ref_it->second;
@@ -136,7 +149,10 @@ std::vector<size_t> CrossReferenceSystem::search(const std::string& text_name, c
   return {};
 }
 
-void CrossReferenceSystem::replace(const std::string& text_name, const std::string& old_word, const std::string& new_word) {
+void CrossReferenceSystem::replace(const std::string &text_name,
+                                   const std::string &old_word,
+                                   const std::string &new_word)
+{
   auto it = texts.find(text_name);
   if (it == texts.end()) {
     throw std::runtime_error("<NOT FOUND>");
@@ -145,7 +161,7 @@ void CrossReferenceSystem::replace(const std::string& text_name, const std::stri
   std::string search_old_word = old_word;
   std::transform(search_old_word.begin(), search_old_word.end(), search_old_word.begin(), ::tolower);
 
-  TextData& data = it->second;
+  TextData &data = it->second;
   auto ref_it = data.references.find(search_old_word);
   if (ref_it == data.references.end()) {
     throw std::runtime_error("<WORD NOT FOUND>");
@@ -166,14 +182,17 @@ void CrossReferenceSystem::replace(const std::string& text_name, const std::stri
 
   std::ostringstream oss;
   for (size_t i = 0; i < words.size(); ++i) {
-    if (i > 0) oss << " ";
+    if (i > 0) {
+      oss << " ";
+    }
     oss << words[i];
   }
 
   buildReferences(text_name, oss.str());
 }
 
-void CrossReferenceSystem::insert(const std::string& text_name, size_t position, const std::string& word) {
+void CrossReferenceSystem::insert(const std::string &text_name, size_t position, const std::string &word)
+{
   if (!isValidWord(word)) {
     throw std::runtime_error("<INVALID WORD>");
   }
@@ -183,7 +202,7 @@ void CrossReferenceSystem::insert(const std::string& text_name, size_t position,
     throw std::runtime_error("<NOT FOUND>");
   }
 
-  TextData& data = it->second;
+  TextData &data = it->second;
   std::istringstream iss(data.content);
   std::vector<std::string> words;
   std::string current_word;
@@ -200,20 +219,23 @@ void CrossReferenceSystem::insert(const std::string& text_name, size_t position,
 
   std::ostringstream oss;
   for (size_t i = 0; i < words.size(); ++i) {
-    if (i > 0) oss << " ";
+    if (i > 0) {
+      oss << " ";
+    }
     oss << words[i];
   }
 
   buildReferences(text_name, oss.str());
 }
 
-void CrossReferenceSystem::remove(const std::string& text_name, size_t start, size_t end) {
+void CrossReferenceSystem::remove(const std::string &text_name, size_t start, size_t end)
+{
   auto it = texts.find(text_name);
   if (it == texts.end()) {
     throw std::runtime_error("<NOT FOUND>");
   }
 
-  TextData& data = it->second;
+  TextData &data = it->second;
   std::istringstream iss(data.content);
   std::vector<std::string> words;
   std::string word;
@@ -230,21 +252,24 @@ void CrossReferenceSystem::remove(const std::string& text_name, size_t start, si
 
   std::ostringstream oss;
   for (size_t i = 0; i < words.size(); ++i) {
-    if (i > 0) oss << " ";
+    if (i > 0) {
+      oss << " ";
+    }
     oss << words[i];
   }
 
   buildReferences(text_name, oss.str());
 }
 
-void CrossReferenceSystem::import(const std::string& filename) {
+void CrossReferenceSystem::import(const std::string &filename)
+{
   std::ifstream infile(filename);
   if (!infile) {
     throw std::runtime_error("<FILE NOT FOUND>");
   }
 
   std::string content((std::istreambuf_iterator<char>(infile)),
-                     std::istreambuf_iterator<char>());
+                      std::istreambuf_iterator<char>());
 
   if (content.empty()) {
     throw std::runtime_error("<INVALID FORMAT>");
@@ -259,7 +284,8 @@ void CrossReferenceSystem::import(const std::string& filename) {
   build(text_name, content);
 }
 
-void CrossReferenceSystem::export_text(const std::string& text_name, const std::string& filename) {
+void CrossReferenceSystem::export_text(const std::string &text_name, const std::string &filename)
+{
   auto it = texts.find(text_name);
   if (it == texts.end()) {
     throw std::runtime_error("<NOT FOUND>");
@@ -270,10 +296,12 @@ void CrossReferenceSystem::export_text(const std::string& text_name, const std::
     throw std::runtime_error("<IO ERROR>");
   }
 
-  for (const auto& ref : it->second.references) {
+  for (const auto &ref : it->second.references) {
     outfile << ref.first << ":";
     for (size_t i = 0; i < ref.second.size(); ++i) {
-      if (i > 0) outfile << ",";
+      if (i > 0) {
+        outfile << ",";
+      }
       outfile << ref.second[i];
     }
     outfile << "\n";
@@ -282,31 +310,35 @@ void CrossReferenceSystem::export_text(const std::string& text_name, const std::
   outfile.close();
 }
 
-void CrossReferenceSystem::stats(const std::string& text_name) {
+void CrossReferenceSystem::stats(const std::string &text_name)
+{
   auto it = texts.find(text_name);
   if (it == texts.end()) {
     throw std::runtime_error("<NOT FOUND>");
   }
 
-  const TextData& data = it->second;
+  const TextData &data = it->second;
   std::cout << "Total words: " << data.references.size() << std::endl;
   std::cout << "Total occurrences: " << data.content.size() << std::endl;
 
   std::vector<std::pair<std::string, size_t>> word_counts;
-  for (const auto& ref : data.references) {
+  for (const auto &ref : data.references) {
     word_counts.emplace_back(ref.first, ref.second.size());
   }
 
   std::sort(word_counts.begin(), word_counts.end(),
-           [](const auto& a, const auto& b) { return a.second > b.second; });
+            [](const auto &a, const auto &b) { return a.second > b.second; });
 
   std::cout << "Top 5 words:" << std::endl;
-  for (size_t i = 0; i < std::min(size_t(5), word_counts.size()); ++i) {
+  for (size_t i = 0; i < std::min<size_t>(5, word_counts.size()); ++i) {
     std::cout << "  " << word_counts[i].first << ": " << word_counts[i].second << std::endl;
   }
 }
 
-void CrossReferenceSystem::merge(const std::string& new_name, const std::string& name1, const std::string& name2) {
+void CrossReferenceSystem::merge(const std::string &new_name,
+                                 const std::string &name1,
+                                 const std::string &name2)
+{
   if (!isValidName(new_name)) {
     throw std::runtime_error("<INVALID NAME>");
   }
@@ -323,8 +355,8 @@ void CrossReferenceSystem::merge(const std::string& new_name, const std::string&
     throw std::runtime_error("<NOT FOUND: " + name2 + ">");
   }
 
-  const TextData& data1 = it1->second;
-  const TextData& data2 = it2->second;
+  const TextData &data1 = it1->second;
+  const TextData &data2 = it2->second;
 
   std::istringstream iss(data1.content);
   size_t word_count1 = 0;
@@ -337,9 +369,9 @@ void CrossReferenceSystem::merge(const std::string& new_name, const std::string&
   new_data.content = data1.content + " " + data2.content;
   new_data.references = data1.references;
 
-  for (const auto& pair : data2.references) {
-    const std::string& word = pair.first;
-    const std::vector<size_t>& positions = pair.second;
+  for (const auto &pair : data2.references) {
+    const std::string &word = pair.first;
+    const std::vector<size_t> &positions = pair.second;
     std::vector<size_t> shifted_positions;
     for (size_t pos : positions) {
       shifted_positions.push_back(pos + word_count1);
@@ -347,7 +379,8 @@ void CrossReferenceSystem::merge(const std::string& new_name, const std::string&
 
     if (new_data.references.find(word) != new_data.references.end()) {
       new_data.references[word].insert(new_data.references[word].end(),
-                                      shifted_positions.begin(), shifted_positions.end());
+                                       shifted_positions.begin(),
+                                       shifted_positions.end());
     } else {
       new_data.references[word] = shifted_positions;
     }
@@ -356,7 +389,8 @@ void CrossReferenceSystem::merge(const std::string& new_name, const std::string&
   texts[new_name] = new_data;
 }
 
-int processCommandLineArguments(int argc, char** argv, CrossReferenceSystem& system) {
+int processCommandLineArguments(int argc, char **argv, CrossReferenceSystem &system)
+{
   if (argc > 1) {
     std::string arg = argv[1];
     if (arg == "--help") {
@@ -367,7 +401,7 @@ int processCommandLineArguments(int argc, char** argv, CrossReferenceSystem& sys
         system.import(argv[2]);
         std::cout << "File is valid" << std::endl;
         return 0;
-      } catch (const std::exception& e) {
+      } catch (const std::exception &e) {
         std::cerr << "Error: " << e.what() << std::endl;
         return 1;
       }
@@ -377,7 +411,7 @@ int processCommandLineArguments(int argc, char** argv, CrossReferenceSystem& sys
       try {
         system.import(argv[1]);
         return 0;
-      } catch (const std::exception& e) {
+      } catch (const std::exception &e) {
         std::cerr << "Error: " << e.what() << std::endl;
         return 1;
       }
@@ -386,7 +420,8 @@ int processCommandLineArguments(int argc, char** argv, CrossReferenceSystem& sys
   return -1;
 }
 
-void runInteractiveMode(CrossReferenceSystem& system) {
+void runInteractiveMode(CrossReferenceSystem &system)
+{
   std::string command;
   while (true) {
     std::cout << "> ";
@@ -408,8 +443,7 @@ void runInteractiveMode(CrossReferenceSystem& system) {
         }
         system.build(name, content);
         std::cout << "OK" << std::endl;
-      }
-      else if (cmd == "reconstruct") {
+      } else if (cmd == "reconstruct") {
         std::string name, output_file;
         iss >> name;
         if (iss >> output_file) {
@@ -418,14 +452,12 @@ void runInteractiveMode(CrossReferenceSystem& system) {
           system.reconstruct(name);
         }
         std::cout << "OK" << std::endl;
-      }
-      else if (cmd == "concat") {
+      } else if (cmd == "concat") {
         std::string new_name, name1, name2;
         iss >> new_name >> name1 >> name2;
         system.concat(new_name, name1, name2);
         std::cout << "OK" << std::endl;
-      }
-      else if (cmd == "search") {
+      } else if (cmd == "search") {
         std::string name, word;
         iss >> name >> word;
         auto positions = system.search(name, word);
@@ -438,57 +470,48 @@ void runInteractiveMode(CrossReferenceSystem& system) {
           }
           std::cout << std::endl;
         }
-      }
-      else if (cmd == "replace") {
+      } else if (cmd == "replace") {
         std::string name, old_word, new_word;
         iss >> name >> old_word >> new_word;
         system.replace(name, old_word, new_word);
         std::cout << "OK" << std::endl;
-      }
-      else if (cmd == "insert") {
+      } else if (cmd == "insert") {
         std::string name, word;
         size_t position;
         iss >> name >> position >> word;
         system.insert(name, position, word);
         std::cout << "OK" << std::endl;
-      }
-      else if (cmd == "remove") {
+      } else if (cmd == "remove") {
         std::string name;
         size_t start, end;
         iss >> name >> start >> end;
         system.remove(name, start, end);
         std::cout << "OK" << std::endl;
-      }
-      else if (cmd == "import") {
+      } else if (cmd == "import") {
         std::string filename;
         iss >> filename;
         system.import(filename);
         std::cout << "OK" << std::endl;
-      }
-      else if (cmd == "export") {
+      } else if (cmd == "export") {
         std::string name, filename;
         iss >> name >> filename;
         system.export_text(name, filename);
         std::cout << "OK" << std::endl;
-      }
-      else if (cmd == "stats") {
+      } else if (cmd == "stats") {
         std::string name;
         iss >> name;
         system.stats(name);
-      }
-      else if (cmd == "merge") {
+      } else if (cmd == "merge") {
         std::string new_name, name1, name2;
         iss >> new_name >> name1 >> name2;
         system.merge(new_name, name1, name2);
         std::cout << "OK" << std::endl;
-      }
-      else if (cmd == "exit" || cmd == "quit") {
+      } else if (cmd == "exit" || cmd == "quit") {
         break;
-      }
-      else {
+      } else {
         std::cout << "Unknown command: " << cmd << std::endl;
       }
-    } catch (const std::exception& e) {
+    } catch (const std::exception &e) {
       std::cerr << "Error: " << e.what() << std::endl;
     }
   }
