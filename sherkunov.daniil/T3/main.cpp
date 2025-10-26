c95e3bd20599453eb350c9e2fa5121c856f87b04
kopinik <bk65763980@mail.ru>
2025-10-27 02:05:28 +0300

    adsf

--- sherkunov.daniil/T3/main.cpp
+++ sherkunov.daniil/T3/main.cpp
@@ -61,8 +61,8 @@ int main(int argc, char** argv)
       {
         std::cin.clear(std::cin.rdstate() ^ std::ios::failbit);
       }
-        std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
-        std::cout << "<INVALID COMMAND>\n";
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+      std::cout << "<INVALID COMMAND>\n";
     }
   }
 }
